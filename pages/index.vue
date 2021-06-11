a86a263bd5f63c55e535ebafe172dfd6e850aaa0
Siroshun09 <siro.shun8@gmail.com>
2021-06-11 12:03:39 +0900

    feat: add empty page

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    This is the homepage of Siroshun09.
+  </div>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
