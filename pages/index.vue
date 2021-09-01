1473b0d13d1e1c0cf1ce580a4a90e58b8b07717b
Siroshun09 <siro.shun8@gmail.com>
2021-09-01 10:13:00 +0900

    clean: cleanup index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,28 @@
 <template>
-  <div class="mx-auto container">
-    <Header/>
-    <div class="text-base m-5 text-gray-800">
-      <h1 class="text-3xl my-2">Hello, I'm Siroshun.</h1>
+  <div>
+    <div class="m-5">
+      <h1>Hello, I'm Siroshun.</h1>
       <p>I'm a Java programmer, mainly developing software for Minecraft.</p>
       <p>I also enjoy creating libraries for Java and learning new programming languages and technologies.</p>
       <br/>
-      <p>I usually use <code class="bg-gray-200 px-1 rounded">Siroshun09</code> for my online id.</p>
+      <p>I usually use <code>Siroshun09</code> for my online id.</p>
       <p>If you want to contact me, please see
-        <NuxtLink class="text-blue-500 hover:text-blue-700" to="contact">this page</NuxtLink>.</p>
+        <NuxtLink to="contact">this page</NuxtLink>.</p>
     </div>
   </div>
 </template>
 
-<script>
-import Header from "@/components/Header";
-
-export default {
-  components: {Header}
-}
-</script>
+<style scoped>
+  h1 {
+    @apply text-3xl my-2 text-black
+  }
+  p {
+    @apply text-base text-gray-800
+  }
+  a {
+    @apply text-blue-600 hover:text-blue-800
+  }
+  code {
+    @apply bg-gray-200 px-1.5 rounded text-black
+  }
+</style>
