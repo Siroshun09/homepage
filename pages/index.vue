23a43a08b6c7737b4d993eee69bc0982d5f2d9ec
Siroshun09 <siro.shun8@gmail.com>
2021-09-01 10:17:45 +0900

    format: reformat index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,23 +6,25 @@
       <p>I also enjoy creating libraries for Java and learning new programming languages and technologies.</p>
       <br/>
       <p>I usually use <code>Siroshun09</code> for my online id.</p>
-      <p>If you want to contact me, please see
-        <NuxtLink to="contact">this page</NuxtLink>.</p>
+      <p>If you want to contact me, please see <NuxtLink to="contact">this page</NuxtLink>.</p>
     </div>
   </div>
 </template>
 
 <style scoped>
-  h1 {
-    @apply text-3xl my-2 text-black
-  }
-  p {
-    @apply text-base text-gray-800
-  }
-  a {
-    @apply text-blue-600 hover:text-blue-800
-  }
-  code {
-    @apply bg-gray-200 px-1.5 rounded text-black
-  }
+h1 {
+  @apply text-3xl my-2 text-black
+}
+
+p {
+  @apply text-base text-gray-800
+}
+
+a {
+  @apply text-blue-600 hover:text-blue-800
+}
+
+code {
+  @apply bg-gray-200 px-1.5 rounded text-black
+}
 </style>
