5e1247ecc9474ccebfd670336f50f373f65677f9
Siroshun09 <siro.shun8@gmail.com>
2026-01-14 01:32:24 +0900

    refactor: use `bg-linear-to-br` instead of `bg-gradient-to-br`

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -2,7 +2,7 @@ import { Card } from "../../components/profile/Card";
 
 export const ProfilePage = () => {
 	return (
-		<main class="h-full bg-gradient-to-br from-indigo-300 to-blue-400 flex justify-center">
+		<main class="h-full bg-linear-to-br from-indigo-300 to-blue-400 flex justify-center">
 			<Card />
 		</main>
 	);
